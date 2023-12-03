8a12164dacbad11dd4c6fce773ec37f67d92313a
RoheSama <roger.companys@enti.cat>
2023-12-03 17:05:48 +0100

    Exercicie 1 setup

--- AA2/MyOctopusLibrary/OctopusController/OctopusController/MyTentacleController.cs
+++ AA2/MyOctopusLibrary/OctopusController/OctopusController/MyTentacleController.cs
@@ -25,19 +25,14 @@ namespace OctopusController
         //Exercise 1.
         public Transform[] LoadTentacleJoints(Transform root, TentacleMode mode)
         {
-            //TODO: add here whatever is needed to find the bones forming the tentacle for all modes
-            //you may want to use a list, and then convert it to an array and save it into _bones
             tentacleMode = mode;
 
             switch (tentacleMode){
                 case TentacleMode.LEG:
-                    //TODO: in _endEffectorsphere you keep a reference to the base of the leg
                     break;
                 case TentacleMode.TAIL:
-                    //TODO: in _endEffectorsphere you keep a reference to the red sphere 
                     break;
                 case TentacleMode.TENTACLE:
-                    //TODO: in _endEffectorphere you  keep a reference to the sphere with a collider attached to the endEffector
                     break;
             }
             return Bones;
